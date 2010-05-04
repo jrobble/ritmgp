dd8c0043124adce24ee5028660db740faf91b180
rekaeuqs1 <rekaeuqs1@2b805aa1-9f5e-261c-bd4e-3cea6d9ac202>
2010-05-04 19:21:07 +0000

    Now opens with no images open, added a "refresh images" button so you don't have to restart the plugin with each new set of images.

--- trunk/ij143s-src/src/ritmgp/RITMGP_PlugIn.java
+++ trunk/ij143s-src/src/ritmgp/RITMGP_PlugIn.java
@@ -15,22 +15,25 @@ import ritmgp.view.MGPInterface;
 public class RITMGP_PlugIn implements PlugIn{
 
     public void run(String string) {
-       int[] wList = WindowManager.getIDList(); //ij.WindowManager
-       if (wList==null || wList.length<2) {
-           IJ.showMessage("Error", "There must be at least two windows open"); //ij.IJ
-           return;
-       }
-       ArrayList<String> titles = new ArrayList<String>(wList.length);
-       int i = 0;
-       for (int imgId : wList) {
-           ImagePlus imp = WindowManager.getImage(imgId);
-           if (imp!=null)
-               titles.add(imp.getTitle());
-       }
+        int[] wList = WindowManager.getIDList(); //ij.WindowManager
+        if (wList == null) {
+            //IJ.showMessage("Error", "There must be at least two windows open"); //ij.IJ
+            MGPInterface inter = new MGPInterface(new String[0]);
+            inter.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            inter.setVisible(true);
+            return;
+        }
+        ArrayList<String> titles = new ArrayList<String>(wList.length);
+        int i = 0;
+        for (int imgId : wList) {
+            ImagePlus imp = WindowManager.getImage(imgId);
+            if (imp != null) {
+                titles.add(imp.getTitle());
+            }
+        }
 
-       MGPInterface inter = new MGPInterface(titles);
-       inter.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-       inter.setVisible(true);
+        MGPInterface inter = new MGPInterface(titles);
+        inter.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        inter.setVisible(true);
     }
-
 }
